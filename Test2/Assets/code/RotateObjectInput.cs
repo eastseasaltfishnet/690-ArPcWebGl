dd5a3d3a01b93ce17829327e9e03e504e876ad3c
Eastsea.Saltfishnet <2209743509@qq.com>
2024-11-15 00:26:05 -0500

    update

--- Test2/Assets/code/RotateObjectInput.cs
+++ Test2/Assets/code/RotateObjectInput.cs
@@ -54,8 +54,6 @@ public class RotateObjectInput : MonoBehaviour
 
                     if (Application.isMobilePlatform)
                     {
-                        rotationX = -rotationX; // 反转平移的左右方向
-                        rotationY = rotationY;
                     }
 
                     // 应用旋转（世界坐标系）
